02031e5aa88ad90337983cc0dc197e3488b32323
RQ <ricky.dwsystems@gmail.com>
2022-02-10 10:32:34 +0700

    update target framework from 4.5.2 to 4.8 || misc improvements

--- LIBWebMVC/WebDBConnection.cs
+++ LIBWebMVC/WebDBConnection.cs
@@ -36,6 +36,21 @@ namespace LIBWebMVC
             database.ExecuteSqlCommand(string.Format("UPDATE {0} SET {1} WHERE {2}", tableName, fields, whereClause), parameters);
         }
 
+        public static void Delete(Database database, string tableName, params SqlParameter[] parameters)
+        {
+            string fields = "";
+            string whereClause = "";
+            foreach (SqlParameter param in parameters)
+            {
+                if (param.ParameterName.ToLower() == "id")
+                    whereClause = string.Format("{0}.{1} = @{1}", tableName, param.ParameterName);
+                else
+                    fields = Util.append(fields, string.Format("{0} = @{0}", param.ParameterName), ", ");
+            }
+
+            database.ExecuteSqlCommand(string.Format("DELETE {0} WHERE {1}", tableName, whereClause), parameters);
+        }
+
         /*******************************************************************************************************/
 
     }
